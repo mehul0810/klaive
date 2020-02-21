96d18fa1470d92f2ca222b9050cd30aaf5206761
Mehul Gohil <mehul.gohil0810@gmail.com>
2020-02-21 20:41:29 +0530

    fix: update code on admin settings file

--- src/admin/Settings.php
+++ src/admin/Settings.php
@@ -1,18 +1,37 @@
 <?php
+/**
+ * Klaviyo for Give | Admin Settings
+ *
+ * @since 1.0.0
+ */
 namespace KlaviyoForGive\Admin;
 
+/**
+ * Class Settings.
+ *
+ * @since 1.0.0
+ *
+ * @package KlaviyoForGive\Admin
+ */
 class Settings {
 
+	/**
+	 * Settings constructor.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 *
+	 * @return void
+	 */
 	public function __construct() {
 		add_filter( 'give_get_sections_addons', array( $this, 'register_sections' ) );
 		add_filter( 'give_get_settings_addons', array( $this, 'register_settings' ) );
 	}
 
-
 	/**
 	 * Register sections.
 	 *
-	 * @since  1.0.3
+	 * @since  1.0.0
 	 * @access public
 	 *
 	 * @param array $sections List of sections.
@@ -25,164 +44,87 @@ class Settings {
 		return $sections;
 	}
 
-
-
 	/**
 	 * Registers the plugin's settings.
 	 *
-	 * @param $settings
+	 * @param array $settings List of settings.
+	 *
+	 * @since  1.0.0
+	 * @access public
 	 *
 	 * @return array
 	 */
 	public function register_settings( $settings ) {
 
-		$get_data = give_clean( $_GET );
+		switch ( give_get_current_setting_section() ) {
 
-		if (
-			'addons' == $get_data['tab'] &&
-			'klaviyo' == $get_data['section']
-		) {
-			$settings = array(
-				array(
-				'id'   => 'give_title_klaviyo',
-					'type' => 'title',
-				),
-				array(
-					'name' => __( 'Klaviyo Settings', 'give-convertkit' ),
-					'desc' => '<hr>',
-					'id'   => 'give_title_',
-					'type' => 'give_title',
-				),
-				array(
-					'id'   => 'give_klaviyo_api_key',
-					'name' => __( 'ConvertKit API Key', 'give-convertkit' ),
-					'desc' => sprintf( __( 'Enter your ConvertKit API key. You may retrieve your ConvertKit API key from your <a href="%s" target="_blank" title="Will open new window">account settings</a>.', 'give-convertkit' ), 'https://app.convertkit.com/account/edit' ),
-					'type' => 'text',
-					'size' => 'regular',
-				),
-				array(
-					'id'      => 'give_klaviyo_show_subscribe_checkbox',
-					'name'    => __( 'Enable Globally', 'give-convertkit' ),
-					'desc'    => __( 'Allow donors to sign up for the forms selected below on all donation forms? Note: the forms(s) can be customized per form.', 'give-convertkit' ),
-					'type'    => 'radio_inline',
-					'default' => 'enabled',
-					'options' => array(
-						'enabled'  => __( 'Enabled', 'give-convertkit' ),
-						'disabled' => __( 'Disabled', 'give-convertkit' ),
+			case 'klaviyo':
+				$settings = array(
+					array(
+						'id'   => 'give_title_klaviyo',
+						'type' => 'title',
 					),
-				),
-				array(
-					'id'   => 'give_klaviyo_list',
-					'name' => __( 'Choose a Form', 'give-convertkit' ),
-					'desc' => __( 'Select the form you wish to subscribe donors to by default.', 'give-convertkit' ),
-					'type' => 'convertkit_list_select',
-				),
-				array(
-					'id'   => '_give_klaviyo_tags',
-					'name' => __( 'Choose Tags', 'give-convertkit' ),
-					'desc' => __( 'Select the tags you wish to subscribe donors to by default.', 'give-convertkit' ),
-					'type' => 'convertkit_tag_list',
-				),
-				array(
-					'id'      => 'give_klaviyo_checked_default',
-					'name'    => __( 'Opt-in Default', 'give-convertkit' ),
-					'desc'    => __( 'Would you like the newsletter opt-in checkbox checked by default? This option can be customized per form.', 'give-convertkit' ),
-					'options' => array(
-						'enabled'  => __( 'Checked', 'give-convertkit' ),
-						'disabled' => __( 'Unchecked', 'give-convertkit' ),
+					array(
+						'name' => __( 'Klaviyo Settings', 'klaviyo-for-give' ),
+						'desc' => '<hr>',
+						'id'   => 'give_title_klaviyo',
+						'type' => 'give_title',
 					),
-					'default' => 'enabled',
-					'type'    => 'radio_inline',
-				),
-				array(
-					'id'         => 'give_klaviyo_label',
-					'name'       => __( 'Default Label', 'give-convertkit' ),
-					'desc'       => __( 'This is the text shown next to the signup option. This can also be customized per form.', 'give-convertkit' ),
-					'type'       => 'text',
-					'size'       => 'regular',
-					'attributes' => array(
-						'placeholder' => __( 'Subscribe to our newsletter', 'give-convertkit' ),
+					array(
+						'id'      => 'klaviyo_for_give_enable_globally',
+						'name'    => __( 'Enable Globally', 'klaviyo-for-give' ),
+						'desc'    => __( 'Allow donors to sign up for the forms selected below on all donation forms?', 'klaviyo-for-give' ),
+						'type'    => 'radio_inline',
+						'default' => 'enabled',
+						'options' => array(
+							'enabled'  => __( 'Enabled', 'klaviyo-for-give' ),
+							'disabled' => __( 'Disabled', 'klaviyo-for-give' ),
+						),
 					),
-				),
-				array(
-					'id'   => 'give_title_klaviyo',
-					'type' => 'sectionend',
-				),
-			);
+					array(
+						'id'   => 'klaviyo_for_give_api_key',
+						'name' => __( 'API Key', 'klaviyo-for-give' ),
+						'desc' => sprintf(
+							__( 'Enter your Klaviyo API key. You may retrieve your Klaviyo API key from your <a href="%s" target="_blank">account settings</a>.', 'klaviyo-for-give' ),
+							esc_url_raw( 'https://www.klaviyo.com/account#api-keys-tab' )
+						),
+						'type' => 'text',
+						'size' => 'regular',
+					),
+					array(
+						'id'   => 'klaviyo_for_give_selected_list',
+						'name' => __( 'Select a List', 'klaviyo-for-give' ),
+						'desc' => __( 'Select the list you wish to set as default for subscribe donors.', 'klaviyo-for-give' ),
+						'type' => 'klaviyo_select_list',
+					),
+					array(
+						'id'      => 'klaviyo_for_give_checked_default',
+						'name'    => __( 'Opt-in Default', 'klaviyo-for-give' ),
+						'desc'    => __( 'Would you like the newsletter opt-in checkbox checked by default?', 'klaviyo-for-give' ),
+						'options' => array(
+							'enabled'  => __( 'Checked', 'klaviyo-for-give' ),
+							'disabled' => __( 'Unchecked', 'klaviyo-for-give' ),
+						),
+						'default' => 'enabled',
+						'type'    => 'radio_inline',
+					),
+					array(
+						'id'         => 'klaviyo_for_give_default_checkbox_text',
+						'name'       => __( 'Default Text', 'klaviyo-for-give' ),
+						'desc'       => __( 'This is the text shown next to the Sign Up Newsletter checkbox. This can also be customized per form.', 'klaviyo-for-give' ),
+						'type'       => 'text',
+						'size'       => 'regular',
+						'attributes' => array(
+							'placeholder' => __( 'Subscribe to our newsletter', 'klaviyo-for-give' ),
+						),
+					),
+					array(
+						'id'   => 'give_title_klaviyo',
+						'type' => 'sectionend',
+					),
+				);
+				break;
 		}
-
-//		switch ( give_get_current_setting_section() ) {
-//
-//			case 'klaviyo':
-//				$settings = array(
-//					array(
-//						'id'   => 'give_title_klaviyo',
-//						'type' => 'title',
-//					),
-//					array(
-//						'name' => __( 'Klaviyo Settings', 'give-convertkit' ),
-//						'desc' => '<hr>',
-//						'id'   => 'give_title_',
-//						'type' => 'give_title',
-//					),
-//					array(
-//						'id'   => 'give_klaviyo_api_key',
-//						'name' => __( 'ConvertKit API Key', 'give-convertkit' ),
-//						'desc' => sprintf( __( 'Enter your ConvertKit API key. You may retrieve your ConvertKit API key from your <a href="%s" target="_blank" title="Will open new window">account settings</a>.', 'give-convertkit' ), 'https://app.convertkit.com/account/edit' ),
-//						'type' => 'text',
-//						'size' => 'regular',
-//					),
-//					array(
-//						'id'      => 'give_klaviyo_show_subscribe_checkbox',
-//						'name'    => __( 'Enable Globally', 'give-convertkit' ),
-//						'desc'    => __( 'Allow donors to sign up for the forms selected below on all donation forms? Note: the forms(s) can be customized per form.', 'give-convertkit' ),
-//						'type'    => 'radio_inline',
-//						'default' => 'enabled',
-//						'options' => array(
-//							'enabled'  => __( 'Enabled', 'give-convertkit' ),
-//							'disabled' => __( 'Disabled', 'give-convertkit' ),
-//						),
-//					),
-//					array(
-//						'id'   => 'give_klaviyo_list',
-//						'name' => __( 'Choose a Form', 'give-convertkit' ),
-//						'desc' => __( 'Select the form you wish to subscribe donors to by default.', 'give-convertkit' ),
-//						'type' => 'convertkit_list_select',
-//					),
-//					array(
-//						'id'   => '_give_klaviyo_tags',
-//						'name' => __( 'Choose Tags', 'give-convertkit' ),
-//						'desc' => __( 'Select the tags you wish to subscribe donors to by default.', 'give-convertkit' ),
-//						'type' => 'convertkit_tag_list',
-//					),
-//					array(
-//						'id'      => 'give_klaviyo_checked_default',
-//						'name'    => __( 'Opt-in Default', 'give-convertkit' ),
-//						'desc'    => __( 'Would you like the newsletter opt-in checkbox checked by default? This option can be customized per form.', 'give-convertkit' ),
-//						'options' => array(
-//							'enabled'  => __( 'Checked', 'give-convertkit' ),
-//							'disabled' => __( 'Unchecked', 'give-convertkit' ),
-//						),
-//						'default' => 'enabled',
-//						'type'    => 'radio_inline',
-//					),
-//					array(
-//						'id'         => 'give_klaviyo_label',
-//						'name'       => __( 'Default Label', 'give-convertkit' ),
-//						'desc'       => __( 'This is the text shown next to the signup option. This can also be customized per form.', 'give-convertkit' ),
-//						'type'       => 'text',
-//						'size'       => 'regular',
-//						'attributes' => array(
-//							'placeholder' => __( 'Subscribe to our newsletter', 'give-convertkit' ),
-//						),
-//					),
-//					array(
-//						'id'   => 'give_title_klaviyo',
-//						'type' => 'sectionend',
-//					),
-//				);
-//				break;
-//		}
 
 		return $settings;
 	}
