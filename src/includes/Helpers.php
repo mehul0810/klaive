564ebae30df07eaa1047447c1b1a2d8269091a8c
Mehul Gohil <mehul.gohil0810@gmail.com>
2020-02-24 15:50:25 +0530

    fix: add helper fn to determine whether to show subscribe checkbox or not

--- src/includes/Helpers.php
+++ src/includes/Helpers.php
@@ -21,3 +21,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 function klaviyo_for_give_get_api_key() {
 	return give_get_option( 'klaviyo_for_give_api_key', false );
 }
+
+/**
+ * This helper function will determine whether to show subscribe checkbox or not.
+ *
+ * @param int $form_id Donation Form ID.
+ *
+ * @since 1.0.0
+ *
+ * @return bool
+ */
+function show_subscribe_checkbox( $form_id ) {
+
+	$display = give_is_setting_enabled( give_get_option( 'klaviyo_for_give_enable_globally', 'enabled' ) );
+
+	if ( $form_id > 0 ) {
+		$display = give_is_setting_enabled( give_get_meta( $form_id, 'klaviyo_for_give_enable_per_form', true ) );
+	}
+
+	return $display;
+}
