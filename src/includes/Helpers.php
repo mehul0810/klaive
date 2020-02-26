c5082dfb8684bc9818e63d6b60c918945132b182
Mehul Gohil <mehul.gohil0810@gmail.com>
2020-02-26 13:47:37 +0530

    fix: improve helpers section for better usage

--- src/includes/Helpers.php
+++ src/includes/Helpers.php
@@ -4,7 +4,7 @@
  *
  * @since 1.0.0
  */
-namespace KlaviyoForGive\Includes\Helpers;
+namespace KlaviyoForGive\Includes;
 
 // Bailout, if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
@@ -12,73 +12,84 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * This helper function is used to get API key.
+ * Trait Helpers
  *
  * @since 1.0.0
  *
- * @return string
+ * @package KlaviyoForGive\Includes
  */
-function get_api_key() {
-	return give_get_option( 'klaviyo_for_give_api_key', false );
-}
+trait Helpers {
 
-/**
- * This helper function will determine whether to show subscribe checkbox or not.
- *
- * @param int $form_id Donation Form ID.
- *
- * @since 1.0.0
- *
- * @return bool
- */
-function show_subscribe_checkbox( $form_id ) {
-
-	$display = give_is_setting_enabled( give_get_option( 'klaviyo_for_give_enable_globally', 'enabled' ) );
-
-	if ( $form_id > 0 ) {
-		$display = give_is_setting_enabled( give_get_meta( $form_id, 'klaviyo_for_give_enable_per_form', true ) );
+	/**
+	 * This helper function is used to get API key.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 *
+	 * @return string
+	 */
+	public static function get_api_key() {
+		return give_get_option( 'klaviyo_for_give_api_key', false );
 	}
 
-	return $display;
-}
+	/**
+	 * This helper function will determine whether to show subscribe checkbox or not.
+	 *
+	 * @param int $form_id Donation Form ID.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 *
+	 * @return bool
+	 */
+	public static function show_subscribe_checkbox( $form_id ) {
 
-/**
- * This helper function will determine whether the subscribe checkbox is checked or not.
- *
- * @param int $form_id Donation Form ID.
- *
- * @since 1.0.0
- *
- * @return bool
- */
-function is_subscribe_checkbox_checked( $form_id ) {
+		$display_globally = give_is_setting_enabled( give_get_option( 'klaviyo_for_give_enable_globally', 'enabled' ) );
+		$display_per_form = $form_id > 0 ?
+			give_is_setting_enabled( give_get_meta( $form_id, 'klaviyo_for_give_enable_per_form', true ) ) :
+			false;
 
-	$display = give_is_setting_enabled( give_get_option( 'klaviyo_for_give_is_checkbox_checked_globally', 'enabled' ) );
-
-	if ( $form_id > 0 ) {
-		$display = give_is_setting_enabled( give_get_meta( $form_id, 'klaviyo_for_give_is_checkbox_checked_per_form', true ) );
+		return $display_per_form ? $display_per_form : $display_globally;
 	}
 
-	return $display;
-}
+	/**
+	 * This helper function will determine whether the subscribe checkbox is checked or not.
+	 *
+	 * @param int $form_id Donation Form ID.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 *
+	 * @return bool
+	 */
+	public static function is_subscribe_checkbox_checked( $form_id ) {
 
-/**
- * This helper function is used to get the text of the subscribe checkbox.
- *
- * @param int $form_id Donation Form ID.
- *
- * @since 1.0.0
- *
- * @return string
- */
-function get_subscribe_checkbox_text( $form_id ) {
+		$is_checked_globally = give_is_setting_enabled( give_get_option( 'klaviyo_for_give_is_checkbox_checked_globally', 'enabled' ) );
+		$is_checked_per_form = $form_id > 0 ?
+			give_is_setting_enabled( give_get_meta( $form_id, 'klaviyo_for_give_is_checkbox_checked_per_form', true ) ) :
+			false;
 
-	$default_text = __( 'Subscribe to our newsletter', 'klaviyo-for-give' );
-	$text         = give_get_option( 'klaviyo_for_give_checkbox_text_globally', $default_text );
-
-	if ( $form_id > 0 ) {
-		$text = give_get_meta( $form_id, 'klaviyo_for_give_checkbox_text_per_form', true );
+		return $is_checked_per_form ? $is_checked_per_form : $is_checked_globally;
 	}
 
-	return $text;
+	/**
+	 * This helper function is used to get the text of the subscribe checkbox.
+	 *
+	 * @param int $form_id Donation Form ID.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 *
+	 * @return string
+	 */
+	public static function get_subscribe_checkbox_text( $form_id ) {
+
+		$default_text  = __( 'Subscribe to our newsletter', 'klaviyo-for-give' );
+		$text_globally = give_get_option( 'klaviyo_for_give_checkbox_text_globally', $default_text );
+		$text_per_form = $form_id > 0 ?
+			give_get_meta( $form_id, 'klaviyo_for_give_checkbox_text_per_form', true ) :
+			false;
+
+		return $text_per_form ? $text_per_form : $text_globally;
+	}
 }
