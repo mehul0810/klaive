d38917c7ef056161df841709ff84aaaae7db6d7e
Mehul Gohil <mehul.gohil0810@gmail.com>
2020-02-28 16:20:13 +0530

    fix: update webpack config to run

--- webpack.config.js
+++ webpack.config.js
@@ -15,143 +15,143 @@ const inProduction = ( 'production' === process.env.NODE_ENV );
 const mode = inProduction ? 'production' : 'development';
 
 const config = {
-    mode,
+	mode,
 
-    entry: {
-        'klaviyo-for-give-admin': [ './assets/src/js/admin/main.js', './assets/src/js/admin/main.scss' ],
-    },
-    output: {
-        path: path.join( __dirname, './assets/dist/' ),
-        filename: 'js/[name].js',
-    },
+	entry: {
+		'klaviyo-for-give-admin': [ './assets/src/js/admin/main.js', './assets/src/css/admin/main.scss' ],
+	},
+	output: {
+		path: path.join( __dirname, './assets/dist/' ),
+		filename: 'js/[name].js',
+	},
 
-    // Ensure modules like magnific know jQuery is external (loaded via WP).
-    externals: {
-        $: 'jQuery',
-        jquery: 'jQuery',
-        lodash: 'lodash',
-    },
-    devtool: ! inProduction ? 'source-map' : '',
-    module: {
-        rules: [
+	// Ensure modules like magnific know jQuery is external (loaded via WP).
+	externals: {
+		$: 'jQuery',
+		jquery: 'jQuery',
+		lodash: 'lodash',
+	},
+	devtool: ! inProduction ? 'source-map' : '',
+	module: {
+		rules: [
 
-            // Use Babel to compile JS.
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-            },
+			// Use Babel to compile JS.
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+			},
 
-            // Expose accounting.js for plugin usage.
-            {
-                test: require.resolve( 'accounting' ),
-                use: [
-                    {
-                        loader: 'expose-loader',
-                        options: 'accounting',
-                    },
-                ],
-            },
+			// Expose accounting.js for plugin usage.
+			{
+				test: require.resolve( 'accounting' ),
+				use: [
+					{
+						loader: 'expose-loader',
+						options: 'accounting',
+					},
+				],
+			},
 
-            // Create RTL styles.
-            {
-                test: /\.css$/,
-                use: [
-                    MiniCSSExtractPlugin.loader,
-                    {
-                        loader: 'style-loader',
-                        options: {
-                            sourceMap: true,
-                        },
-                    },
-                ],
-            },
+			// Create RTL styles.
+			{
+				test: /\.css$/,
+				use: [
+					MiniCSSExtractPlugin.loader,
+					{
+						loader: 'style-loader',
+						options: {
+							sourceMap: true,
+						},
+					},
+				],
+			},
 
-            // SASS to CSS.
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCSSExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            sourceMap: true,
-                        },
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: true,
-                            outputStyle: ( inProduction ? 'compressed' : 'nested' ),
-                        },
-                    } ],
-            },
+			// SASS to CSS.
+			{
+				test: /\.scss$/,
+				use: [
+					MiniCSSExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+						options: {
+							sourceMap: true,
+						},
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							sourceMap: true,
+							outputStyle: ( inProduction ? 'compressed' : 'nested' ),
+						},
+					} ],
+			},
 
-            // Font files.
-            {
-                test: /\.(ttf|otf|eot|woff(2)?)(\?[a-z0-9]+)?$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: 'fonts/[name].[ext]',
-                            publicPath: '../',
-                        },
-                    },
-                ],
-            },
+			// Font files.
+			{
+				test: /\.(ttf|otf|eot|woff(2)?)(\?[a-z0-9]+)?$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: 'fonts/[name].[ext]',
+							publicPath: '../',
+						},
+					},
+				],
+			},
 
-            // Image files.
-            {
-                test: /\.(png|jpe?g|gif|svg)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: 'images/[name].[ext]',
-                            publicPath: '../',
-                        },
-                    },
-                ],
-            },
-        ],
-    },
+			// Image files.
+			{
+				test: /\.(png|jpe?g|gif|svg)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: 'images/[name].[ext]',
+							publicPath: '../',
+						},
+					},
+				],
+			},
+		],
+	},
 
-    // Plugins. Gotta have em'.
-    plugins: [
+	// Plugins. Gotta have em'.
+	plugins: [
 
-        // Removes the "dist" folder before building.
-        new CleanWebpackPlugin( [ 'assets/dist' ] ),
+		// Removes the "dist" folder before building.
+		new CleanWebpackPlugin( [ 'assets/dist' ] ),
 
-        new MiniCSSExtractPlugin( {
-            filename: 'css/[name].css',
-        } ),
+		new MiniCSSExtractPlugin( {
+			filename: 'css/[name].css',
+		} ),
 
-        new CopyWebpackPlugin( [ { from: 'assets/src/images', to: 'images' } ] ),
-    ],
+		new CopyWebpackPlugin( [ { from: 'assets/src/images', to: 'images' } ] ),
+	],
 };
 
 if ( inProduction ) {
-    // Create RTL css.
-    config.plugins.push( new WebpackRTLPlugin( {
-        suffix: '-rtl',
-        minify: true,
-    } ) );
+	// Create RTL css.
+	config.plugins.push( new WebpackRTLPlugin( {
+		suffix: '-rtl',
+		minify: true,
+	} ) );
 
-    // Minify images.
-    // Must go after CopyWebpackPlugin above: https://github.com/Klathmon/imagemin-webpack-plugin#example-usage
-    config.plugins.push( new ImageminPlugin( { test: /\.(jpe?g|png|gif|svg)$/i } ) );
+	// Minify images.
+	// Must go after CopyWebpackPlugin above: https://github.com/Klathmon/imagemin-webpack-plugin#example-usage
+	config.plugins.push( new ImageminPlugin( { test: /\.(jpe?g|png|gif|svg)$/i } ) );
 
-    // POT file.
-    wpPot( {
-        package: 'KlaviyoForGive',
-        domain: 'klaviyo-for-give',
-        destFile: 'languages/klaviyo-for-give.pot',
-        relativeTo: './',
-        src: [ './**/*.php', '!./includes/libraries/**/*', '!./vendor/**/*' ],
-        bugReport: 'https://github.com/mehul0810/klaviyo-for-give/issues/new',
-        team: 'Mehul Gohil <hello@mehulgohil.com>',
-    } );
+	// POT file.
+	wpPot( {
+		package: 'KlaviyoForGive',
+		domain: 'klaviyo-for-give',
+		destFile: 'languages/klaviyo-for-give.pot',
+		relativeTo: './',
+		src: [ './**/*.php', '!./includes/libraries/**/*', '!./vendor/**/*' ],
+		bugReport: 'https://github.com/mehul0810/klaviyo-for-give/issues/new',
+		team: 'Mehul Gohil <hello@mehulgohil.com>',
+	} );
 }
 
 module.exports = config;
