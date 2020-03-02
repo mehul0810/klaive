4aa1605ccf402a64d0a5897516e1b0852ec058c9
Mehul Gohil <mehul.gohil0810@gmail.com>
2020-03-02 14:08:01 +0530

    fix: load admin assets as per the requirement

--- src/admin/Actions.php
+++ src/admin/Actions.php
@@ -31,6 +31,7 @@ class Actions {
 	 * @return void
 	 */
 	public function __construct() {
+		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_assets' ), 10 );
 		add_action( 'give_admin_field_klaviyo_select_list', array( $this, 'klaviyo_select_list' ), 10, 2 );
 	}
 
@@ -82,6 +83,30 @@ class Actions {
 		<?php
 		echo ob_get_clean();
 	}
+
+	/**
+	 * Enqueue Admin Assets.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 *
+	 * @return void
+	 */
+	public function enqueue_admin_assets() {
+
+		wp_enqueue_style(
+			'klaviyo-for-give-admin',
+			KLAVIYO_FOR_GIVE_PLUGIN_URL . 'assets/dist/css/admin.css'
+		);
+
+		wp_enqueue_script(
+			'klaviyo-for-give-admin',
+			KLAVIYO_FOR_GIVE_PLUGIN_URL . 'assets/dist/js/admin.js',
+			'',
+			KLAVIYO_FOR_GIVE_VERSION
+		);
+	}
+
 
 }
 
